68400c12126e982e4247c61aff1534586f661bdf
Briana Itzel Preciado <biprecia@circinus-1.ics.uci.edu>
2024-02-19 18:42:26 -0800

    removed redundancy when filtering stop words. filters out numeric strings. 2021 was a popular year lol

--- scraper.py
+++ scraper.py
@@ -3,29 +3,35 @@ from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
-MAX_URL_CONTENT_LENGTH = 5 * 1024 * 1024  # one megabyte
+# 2 megabytes. average kindle e-book is 2.6 MB
+MAX_URL_CONTENT_LENGTH = 2 * 5 * 1024 * 1024
 
-visited_urls = []
-words_dict = defaultdict(int)
+# Q1 http://www.ics.uci.edu#aaa and http://www.ics.uci.edu#bbb are the same URL
+unique_pages = []
+
+# Q2 Used to get 50 most common words
+word_list = defaultdict(int)
+
+# Q3 longest url and word count
 longest_url = ('', 0)
-subdomains = defaultdict(int)
-visit_count = defaultdict(int)  # Map paths to times visited
 
-STOP_WORDS = {'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't",
-              'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
-              "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't",
-              'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have',
-              "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him',
-              'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't",
-              'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor',
-              'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out',
-              'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some',
-              'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there',
-              "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to',
-              'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were',
-              "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's",
-              'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're",
-              "you've", 'your', 'yours', 'yourself', 'yourselves'}
+# Q4  Map ics subdomains to number of unique pages detected in each subdomain
+ics_subdomains = defaultdict(int)
+
+# Map paths to times visited
+visit_count = defaultdict(int)
+
+
+# Read stop words from file
+def get_stopwords(file_path, stop_words):
+    with open(file_path) as file:
+        stop_words.update(map(str.strip, file))
+
+file_path = 'stopwords.txt'
+STOP_WORDS = set()
+
+get_stopwords(file_path, STOP_WORDS)
+
 
 
 def scraper(url, resp):
@@ -37,21 +43,46 @@ def scraper(url, resp):
 def extract_next_links(url, resp):
     links = []
 
-    if url in visited_urls:
+    if url in unique_pages:
         return links
 
     try:
         if resp.status == 200 and len(resp.raw_response.content) < MAX_URL_CONTENT_LENGTH:
-            visited_urls.append(url)
+            unique_pages.append(url)
             parse_url = urlparse(url)
-            extract_content(url, resp)
+
             soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+
+            try:
+                tokens_list = tokenize_content(soup.text)
+
+                if len(tokens_list) > 10:
+
+                    global longest_url
+                    if len(tokens_list) > longest_url[1]:
+                        longest_url = (url, len(tokens_list))
+                    for token in tokens_list:
+                        word_list[token] += 1
+
+                    parse_url = urlparse(url)
+
+                    # reconstructed subdomain url. adds back https or http
+                    subdomain_key = f"{parse_url.scheme}://{parse_url.netloc}"
+
+                    if "ics.uci.edu" in parse_url.netloc:
+                        ics_subdomains[subdomain_key] = ics_subdomains.get(subdomain_key, 0) + 1
+
+            except Exception as e:
+                print(f'Error extracting content from {url}: {e}')
+
             for new_url in soup.find_all('a'):
-                absolute_link = urldefrag(urljoin(parse_url.scheme + "://" + parse_url.netloc, new_url['href']))[0]
-                if '?' in absolute_link:
-                    absolute_link = absolute_link.split("?")[0]
-                if not max_visits(absolute_link):
-                    links.append(absolute_link)
+                # defragmented link
+                absolute_url= urldefrag(urljoin(parse_url.scheme + "://" + parse_url.netloc, new_url['href']))[0]
+                if '?' in absolute_url:
+                    absolute_url = absolute_url.split("?")[0]
+                if not max_visits(absolute_url):
+                    links.append(absolute_url)
     except Exception as e:
         print(f'Error getting: {url}: {e}')
 
@@ -66,11 +97,10 @@ def is_valid(url):
 
         if re.match(pattern, url.lower()):  # Checks if URL matches the requirements
 
-
             # Avoid pdfs and zip attachments
             if "zip-attachment" in parsed.path.lower():
                 return False
-            
+
             if "pdf" in parsed.path:
                 return False
 
@@ -92,48 +122,25 @@ def is_valid(url):
         raise
 
 
-def extract_content(url, resp):
-    try:
-        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-
-        tokens_list = tokenize_content(soup.text)
-
-        if len(tokens_list) > 0:
-            filtered_tokens = [token for token in tokens_list if token not in STOP_WORDS]
-            global longest_url
-            if len(filtered_tokens) > longest_url[1]:
-                longest_url = (url, len(filtered_tokens))
-            for token in filtered_tokens:
-                words_dict[token] += 1
-
-            parse_url = urlparse(url)
-            subdomain_key = f"{parse_url.scheme}://{parse_url.netloc}"
-
-            if "ics.uci.edu" in parse_url.netloc:
-                    subdomains[subdomain_key] = subdomains.get(subdomain_key, 0) + 1
-
-    except Exception as e:
-        print(f'Error extracting content from {url}: {e}')
-
-
 def tokenize_content(text):
-
-    global STOP_WORDS
+    
     tokens = []
     for line in text.split('\n'):
         # lower case
         words = re.findall(r'[A-Za-z0-9]+(?:[\.\'\’\‘][A-Za-z0-9]+)*', line.lower())
-        words = [word for word in words if (word and word not in STOP_WORDS and len(word) > 1)]
+
+        # filter out stop words and numbers
+        words = [word for word in words if (word and len(word) > 1 and not word.isnumeric() and word not in STOP_WORDS)]
         tokens.extend(words)
     return tokens
 
 
-
-def max_visits(link):
-    new_link = urldefrag(link)[0] if urldefrag(link)[0] else link
+# limits the amount of times a unique page can be visited to 10. Used instead of fingerprinting, simhash, etc
+def max_visits(url):
+    new_link = urldefrag(url)[0] if urldefrag(url)[0] else url
     if new_link not in visit_count:
         visit_count[new_link] = 1
-    elif visit_count[new_link] < 15:
+    elif visit_count[new_link] < 10:
         visit_count[new_link] += 1
     else:
         return True
@@ -142,25 +149,25 @@ def max_visits(link):
 
 def output_to_txt():
     with open('output.txt', 'w', encoding='utf-8') as file:
-        file.write("QUESTION #1" + "\n\n")
-        file.write(f"Visited URLs: {len(visited_urls)}\n")
-        for visited_url in visited_urls:
+        file.write("QUESTION #1" + "\n")
+        file.write(f"Unique Pages: {len(unique_pages)}\n")
+        for visited_url in unique_pages:
             file.write(visited_url + '\n')
 
-        file.write("QUESTION #2" + "\n\n")
-        file.write("\nWord Frequencies:\n")
-        sorted_words = sorted(words_dict.items(), key=lambda x: x[1], reverse=True)
+        file.write("\nQUESTION #2" + "\n")
+        file.write("\n50 Most Common Words:\n")
+        sorted_words = sorted(word_list.items(), key=lambda x: x[1], reverse=True)
 
         for word, frequency in enumerate(sorted_words[:50], start=1):
             file.write(f"{word}: {frequency}\n")
 
-        file.write("QUESTION #3" + "\n\n")
+        file.write("\nQUESTION #3" + "\n")
         file.write("\nLongest URL:\n")
         file.write(f"URL: {longest_url[0]}\n")
         file.write(f"Word Count: {longest_url[1]}\n")
 
-        file.write("QUESTION #4" + "\n\n")
-        file.write(f"\nSubdomains in the ics.uci.edu domain: {len(subdomains)}\n")
-        sorted_subdomains = sorted(subdomains.items())
+        file.write("\nQUESTION #4" + "\n")
+        file.write(f"\nSubdomains in the ics.uci.edu domain: {len(ics_subdomains)}\n")
+        sorted_subdomains = sorted(ics_subdomains.items())
         for subdomain, count in sorted_subdomains:
             file.write(f"{subdomain}: {count}\n")
